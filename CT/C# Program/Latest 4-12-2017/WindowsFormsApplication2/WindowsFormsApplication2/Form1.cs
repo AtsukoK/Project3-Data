a80f1d74d098670a199aac91ec5b3e63f316383a
AtsukoK <lichungtai@hotmail.nl>
2017-04-12 16:43:37 +0200

    added latest version program with added button

---
+++ CT/C# Program/Latest 4-12-2017/WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,91 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.chart1.Series["Procent"].Points.AddXY("j2006", "locatie");
+
+            string constring = "datasource=localhost;port=3306;username=root;password=2048";
+            MySqlConnection conDataBase = new MySqlConnection(constring);
+            MySqlCommand cmdDatabase = new MySqlCommand(" Select * from veiligheidsindex.statistieken ;", conDataBase);
+            MySqlDataReader myReader;
+            try
+            {
+                conDataBase.Open();
+                myReader = cmdDatabase.ExecuteReader();
+
+                while (myReader.Read())
+                {
+                    this.chart1.Series["Procent"].Points.AddXY(myReader.GetString("locatie"), myReader.GetInt32("j2006"));
+                }
+            }
+            finally
+            {
+
+            }
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+
+        {
+            this.chart1.Series["Procent"].Points.AddXY("j2006", "locatie");
+
+            string constring = "datasource=localhost;port=3306;username=root;password=2048";
+            MySqlConnection conDataBase = new MySqlConnection(constring);
+            MySqlCommand cmdDatabase = new MySqlCommand(" Select * from veiligheidsindex.statistieken ;", conDataBase);
+            MySqlDataReader myReader;
+            try
+            {
+                conDataBase.Open();
+                myReader = cmdDatabase.ExecuteReader();
+
+                while (myReader.Read())
+                {
+                    this.chart1.Series["Procent"].Points.AddXY(myReader.GetString("regio"), myReader.GetInt32("j2006"));
+                }
+            }
+
+            finally
+            {
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+            {
+
+            }
+        }
+    }
+}
